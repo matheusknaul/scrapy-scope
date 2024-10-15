de9fe1f64990cdecd484072727c8e09e8fd926a3
matheusknaul <contatomatheush0909@gmail.com>
2024-10-15 16:42:05 -0300

    add

--- app/standards/padroes.py
+++ app/standards/padroes.py
@@ -14,10 +14,11 @@ PATTERN_9 = r"([A-Z]{2,4}\s){1,4}\d{2,5}+-\d" # Ex ABNT NBR ISO 7206-4
 PATTERN_10 = r"([A-Z]{2,4}\s){1,4}\d{2,5}+-\d+:\d{4}" # Ex ABNT NBR ISO 7206-4:2024
 PATTERN_11 = r"([A-Z]{2,4}\s){1,4}\d{2,5}+-\d+:\d{4}\sEmenda\s\d{1}+:\d{2,4}" # Ex ABNT NBR ISO 7206-4:2012 Emenda 1:2016
 PATTERN_12 = r"([A-Z]{2,4}\s){1,4}\d{2,5}\s/\s\d{4}\sParte\s\d" #Ex ABNT NBR ISO 17025 / 2025 Parte 3 ()
+PATTERN_13 = r"([A-Z]{2,4}\s){1,4}\d{2,5}+-\d+:\d{4}" # Ex ABNT NBR ISO 7206-4:2024
 # ASTM
 PATTERN_15 = r"([A-Z]{4}\s)[A-Z]{1}\d{2,5}" # Ex ASTM E112
 PATTERN_16 = r"([A-Z]{4}\s)[A-Z]{1}\d{2,5}+:\d{2,4}" # Ex ASTM E112:2022
-PATTERN_17 = r"([A-Z]{4}\s)[A-Z]{1}\d{2,5}\s/\s\d{4}[a-z]{1}\d{1}"
+PATTERN_17 = r"([A-Z]{4}\s)[A-Z]{1}\d{2,5}\s/\s\d{4}[a-z]{1}\d{1}" # Ex ASTM F1264 /2016e1
 
 list_of_patterns = [
                     PATTERN_1, PATTERN_2, PATTERN_3, PATTERN_4, PATTERN_5,
